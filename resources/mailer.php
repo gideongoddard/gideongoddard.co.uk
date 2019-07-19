374cd5877487eae7ef256e889438c9791de1ef9f
Gideon Goddard <goddard.gcw@gmail.com>
2019-07-19 21:52:14 +0100

    Changed email address in mailer

--- resources/mailer.php
+++ resources/mailer.php
@@ -20,8 +20,7 @@
         }
 
         // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
+        $recipient = "goddard.gcw@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
